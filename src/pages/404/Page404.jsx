9e4e84619bba1498c05c7448782a3b0d341eeb8a
Mohammad Waris <mdtech9900@gmail.com>
2023-03-04 22:32:01 +0530

    pageNotFound

--- src/pages/404/Page404.jsx
+++ src/pages/404/Page404.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import "./page404.scss"
+import React from "react";
 
-const Page404 = () => {
-  return (
-    <div> Page404</div>
-  )
-}
+import "./page404.scss";
 
-export default Page404
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
+const PageNotFound = () => {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default PageNotFound;
